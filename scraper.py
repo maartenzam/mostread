8cc93d621e5b859d38d67da79f8297768acaceae
Maarten <maarten@maartens-mbp.atlantis.local>
2016-10-10 12:46:52 +0200

    First code, copied from Scraperwiki

--- scraper.py
+++ scraper.py
@@ -22,3 +22,128 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+#!/usr/bin/env python
+
+import scraperwiki
+import requests
+import lxml.html
+import datetime
+
+#demorgen.be
+html = scraperwiki.scrape("http://www.demorgen.be")
+root = lxml.html.fromstring(html)
+
+data = {}
+
+timestamp = {
+    'source': 'timestamp',
+    'rank': 0,
+    'url': '',
+    'title': datetime.datetime.utcnow().isoformat(),
+    'key': 'timestamp'
+    }
+scraperwiki.sql.save(['key'], timestamp)
+
+#demorgen.be
+for ind,el in enumerate(root.cssselect(".widget-most-recent ol li a"), 1):
+    if ind > 5: break
+    key = 'demorgen' + str(ind)
+    
+    url = el.attrib['href']
+    
+    data[ind] = {
+        'source': 'demorgen.be',
+        'rank': ind,
+        'url': el.attrib['href'],
+        'title': root.cssselect(".widget-most-recent ol li a span")[ind-1].text,
+        'key': key
+        }
+    scraperwiki.sql.save(['key'], data[ind])
+
+#hln.be
+html = scraperwiki.scrape("http://www.hln.be")
+root = lxml.html.fromstring(html)
+
+data = {}
+
+for ind,el in enumerate(root.cssselect("#hdr_hvdn_top_list a"), 1):
+    key = 'hln' + str(ind)
+    data[ind] = {
+        'source': 'hln.be',
+        'rank': ind,
+        'url': 'http://www.hln.be' + el.attrib['href'],
+        'title': el.text.strip(),
+        'key': key
+        }
+    scraperwiki.sql.save(['key'], data[ind])
+
+#standaard.be
+html = scraperwiki.scrape("http://www.standaard.be")
+root = lxml.html.fromstring(html)
+
+data = {}
+
+for ind,el in enumerate(root.cssselect("#most-read ol li a"), 1):
+    key = 'standaard' + str(ind)
+    data[ind] = {
+        'source': 'standaard.be',
+        'rank': ind,
+        'url': el.attrib['href'],
+        'title': el.text.strip(),
+        'key': key
+        }
+    scraperwiki.sql.save(['key'], data[ind])
+    
+#knack.be
+html = scraperwiki.scrape("http://www.knack.be")
+root = lxml.html.fromstring(html)
+
+data = {}
+
+for ind,el in enumerate(root.cssselect(".most-read.widget ol li a"), 1):
+    key = 'knack' + str(ind)
+    data[ind] = {
+        'source': 'knack.be',
+        'rank': ind,
+        'url': el.attrib['href'],
+        'title': el.attrib['title'],
+        'key': key
+        }
+    scraperwiki.sql.save(['key'], data[ind])
+    
+#nieuwsblad.be
+html = scraperwiki.scrape("http://www.nieuwsblad.be")
+root = lxml.html.fromstring(html)
+
+data = {}
+
+for ind,el in enumerate(root.cssselect('[data-mht-widget="list-|-most-read---overall"] .widget--most-read a'), 1):
+    key = 'nieuwsblad' + str(ind)
+    data[ind] = {
+        'source': 'nieuwsblad.be',
+        'rank': ind,
+        'url': el.attrib['href'],
+        'title': root.cssselect('[data-mht-widget="list-|-most-read---overall"] .widget--most-read a h1')[ind-1].text.strip(),
+        'key': key
+        }
+    scraperwiki.sql.save(['key'], data[ind])
+    
+
+#deredactie.be
+html = scraperwiki.scrape("http://deredactie.be/cm/vrtnieuws")
+root = lxml.html.fromstring(html)
+
+data = {}
+
+for ind,el in enumerate(root.cssselect('.divider.list.boxedlist .contentitem h3 a'), 1):
+    key = 'deredactie' + str(ind)
+    data[ind] = {
+        'source': 'deredactie.be',
+        'rank': ind,
+        'url': 'http://www.deredactie.be' + el.attrib['href'],
+        'title': el.attrib['title'].strip(),
+        'key': key
+        }
+    scraperwiki.sql.save(['key'], data[ind])
+
